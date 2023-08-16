51c44a9f6a16c2395829ccb8153f8043a6cae907
AmaraCela <amaracela@gmail.com>
2023-08-16 21:06:49 +0200

    rewards and traps added

--- Our Project/Assets/Scripts/PlayerMovement.cs
+++ Our Project/Assets/Scripts/PlayerMovement.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private BoxCollider2D coll;
     private Animator anim;
     private float dirX = 0f;
     private SpriteRenderer sprite;
     [SerializeField]private float moveSpeed = 7f;
     [SerializeField]private float jumpForce = 14f;
+    [SerializeField] private LayerMask jumpableGround;
 
     private enum MovementState {idle, running, jumping, falling };
 
@@ -20,6 +22,7 @@ public class PlayerMovement : MonoBehaviour
         rb =  GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         sprite = GetComponent<SpriteRenderer>();
+        coll = GetComponent<BoxCollider2D>();
 
     }
 
@@ -29,7 +32,7 @@ public class PlayerMovement : MonoBehaviour
         dirX = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(moveSpeed * dirX, rb.velocity.y);
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb.velocity = new Vector3(rb.velocity.x,jumpForce,0);
         }
@@ -67,5 +70,10 @@ public class PlayerMovement : MonoBehaviour
         anim.SetInteger("state",(int)state);
     }
 
+
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size,0f, Vector2.down, .1f,jumpableGround);
+    }
 
 }
